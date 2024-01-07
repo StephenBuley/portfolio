ea16c37358f839837d747865ea2285d4449bb67e
Stephen Buley <stephen.a.buley@gmail.com>
2024-01-07 17:28:59 +0100

    add paragraph and portion of readme

--- README.md
+++ README.md
@@ -1,1 +1,3 @@
 # portfolio
+
+This is the code for the portfolio section of my website [stephenbuley.com](https://stephenbuley.com).
--- src/App.tsx
+++ src/App.tsx
@@ -4,6 +4,10 @@ function App() {
   return (
     <>
       <h1>Stephen Buley - Software Developer</h1>
+      <p>
+        I enjoy web developement and am always hungry to learn something new or
+        interesting.
+      </p>
     </>
   )
 }
